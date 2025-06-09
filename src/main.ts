857d2d5b91b8dcef26d5509a675e2d96c2315670
Rajesh M <20039893+RajSM139@users.noreply.github.com>
2025-06-09 21:16:07 +0530

    feat: [XXXX] - added swagger to the application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import './firebase-admin-init';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('TrackExpense API')
+    .setDescription('API documentation for the TrackExpense backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
